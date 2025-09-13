81e7a886ceead733c201ad96d08b5d58ca947465
CrystoferAT <crystofer.tre@cs.up.edu.br>
2025-09-13 17:39:33 -0300

    Mudancas feitas na tela Account

--- app/src/main/java/com/example/urber/Account.kt
+++ app/src/main/java/com/example/urber/Account.kt
@@ -1,9 +1,11 @@
 package com.example.urber
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,21 +16,28 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import org.w3c.dom.Text
 
 @Preview (showBackground = true)
 @Composable
@@ -44,7 +53,7 @@ fun AccountScreen(){
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState())
         ) {
-
+            AccountBody()
         }
 
     }
@@ -70,7 +79,7 @@ fun AccountHeader(){
                     .height(80.dp),
                 verticalArrangement = Arrangement.Center
             ) {
-                Text("José Farah", style = MaterialTheme.typography.titleLarge
+                Text("José Farah", style = MaterialTheme.typography.displayMedium
                     .copy(fontWeight = FontWeight.Bold))
                 Row {
                     Card(
@@ -128,6 +137,70 @@ fun AccountHeader(){
                 modifier = Modifier
                     .size(60.dp)
             )
+        }
+    }
+}
+
+@Composable
+fun AccountBody(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    )
+    {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+
+            verticalAlignment = Alignment.CenterVertically
+        )
+        {
+            AcountSmallCards(
+                w = 100.dp,
+                h = 100.dp,
+                imageVector = Icons.Default.Call  ,
+                text = "Ajuda"
+            )
+            AcountSmallCards(
+                w = 100.dp,
+                h = 100.dp,
+                imageVector = Icons.Default.Menu  ,
+                text = "Carteira"
+            )
+            AcountSmallCards(
+                w = 100.dp,
+                h = 100.dp,
+                imageVector = Icons.Default.Email  ,
+                text = "Mensagem"
+            )
+        }
+    }
+}
+@Composable
+fun AcountSmallCards(w: Dp, h: Dp, imageVector: ImageVector, text: String){
+    Card (
+        modifier = Modifier
+            .size(width = w, height = h ),
+        colors = CardDefaults.cardColors(containerColor = Color.LightGray),
+        shape = RoundedCornerShape(8.dp)
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                imageVector = imageVector,
+                contentDescription = text,
+            )
+            Text(text = text)
         }
     }
 }
---
+++ app/src/main/java/com/example/urber/Home.kt
@@ -0,0 +1,55 @@
+package com.example.urber
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview (showBackground = true)
+@Composable
+fun HomeScreen(){
+    Scaffold (
+        topBar = { HomeHeader() },
+        bottomBar = { Footer() }
+    ) {
+            innerPadding ->
+
+        Column (
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        ) {
+            HomeBody()
+        }
+
+    }
+}
+@Composable
+fun HomeHeader(){
+    Row (modifier =  Modifier.fillMaxWidth()
+        .height(70.dp)
+        .padding(10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            "Uber", style = MaterialTheme.typography.displayMedium,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+@Composable
+fun HomeBody(){
+
+}
