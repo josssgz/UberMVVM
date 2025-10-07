45c7931d4e0dbceb54e96a35de5c22dcc30cdb6e
CRYSTOFER ALONCO TRE <crystofer.tre@cs.up.edu.br>
2025-10-07 10:45:21 -0300

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -61,4 +62,15 @@ dependencies {
 
     implementation(libs.androidx.navigation.compose)
     implementation("androidx.compose.material3:material3:1.2.0")
+
+    val room_version = "2.8.0"
+
+    implementation("androidx.room:room-runtime:$room_version")
+
+    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
+    // See Add the KSP plugin to your project
+    ksp("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
 }
